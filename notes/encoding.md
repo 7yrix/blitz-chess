545f9b57e5d78eb8e4ce5ebc4d52773f339308ef
Moritz Utcke <letsmoe1404@gmail.com>
2024-03-30 17:48:50 +0700

    Fix build-run.sh

---
+++ notes/encoding.md
@@ -0,0 +1,8 @@
+# Encoding
+
+
+Bit Index | Usage
+----------|---------------------------------------------------------------------------------
+0         | Color (1 = Black, 0 = White)
+1-3       | Character type (1 = Pawn, 2 = Knight, 3 = Bishop, 4 = Rook, 5 = Queen, 6 = King)
+4-10      | Position (0-63) (0 = A1, 63 = H8)
--- src/gui/build-run.sh
+++ src/gui/build-run.sh
@@ -1,6 +1,7 @@
 if  g++ -o test Game.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio; then
     # it actually compiled, yippee! now run the game
     ./test
+else
     echo "Compilation has failed. Did you install lsfml?"
     echo "if that does not fix it run 'g++ -o test main.cpp MainMenu.cpp Game.cpp -lsfml-graphics -lsfml-window -lsfml-system -v' for debugging."
 fi
